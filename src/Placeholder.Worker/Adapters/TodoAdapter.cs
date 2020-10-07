9a323cd91845c5e8546a123f5a267e417886003c
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-10-06 23:31:17 -0300

    feat: Create Adapter for EFCore and Todos

---
+++ src/Placeholder.Worker/Adapters/TodoAdapter.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+
+using Placeholder.Domain.Entities;
+using Placeholder.Domain.Repositories;
+using Placeholder.Worker.Storage;
+
+namespace Placeholder.Worker.Adapters
+{
+    internal class TodoAdapter : ITodoRepository
+    {
+        private readonly WorkerContext _context;
+
+        public TodoAdapter(WorkerContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public Task Delete(int userId, int todoId, CancellationToken ct = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Todo> Get(int userId, int todoId, CancellationToken ct = default)
+        {
+            var result = await _context.Todos.SingleOrDefaultAsync(t =>
+                t.UserId == userId &&
+                t.Id == todoId,
+                cancellationToken: ct) ?? Todo.Empty;
+            return result;
+        }
+
+        public async Task<IEnumerable<Todo>> GetAll(CancellationToken ct = default)
+        {
+            var result = await _context.Todos.ToListAsync(ct);
+            return result;
+        }
+
+        public async Task<Todo> Save(Todo todo, CancellationToken ct = default)
+        {
+            var found = await Get(todo.UserId, todo.Id, ct);
+            if (found == Todo.Empty)
+            {
+                found = todo;
+            }
+
+            found.Title = todo.Title;
+            found.Completed = todo.Completed;
+            await _context.SaveChangesAsync(ct);
+            return found;
+        }
+    }
+}
