015e4de6374e9f3a6b11aeff698decc186585c87
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-10-06 22:07:24 -0300

    chore: Scaffold Domain

---
+++ src/Placeholder.Domain/Entities/Todo.cs
@@ -0,0 +1,34 @@
+﻿namespace Placeholder.Domain.Entities
+{
+    public class Todo
+    {
+        public int UserId { get; set; } = 0;
+
+        public int Id { get; set; } = 0;
+
+        public string Title { get; set; } = string.Empty;
+
+        public bool Completed { get; set; } = false;
+
+        public static Todo Empty { get; } = new EmptyTodo();
+    }
+
+    class EmptyTodo : Todo
+    {
+        private const int _unidentifiedId = -1;
+        private const string _unidentifiedKey = "Empty";
+
+        public EmptyTodo()
+        {
+            UserId = _unidentifiedId;
+            Id = _unidentifiedId;
+            Title = _unidentifiedKey;
+            Completed = false;
+        }
+    }
+
+    public class AnotatedTodo : Todo
+    {
+        public string Observation { get; set; } = string.Empty;
+    }
+}
---
+++ src/Placeholder.Domain/Repositories/ITodoRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Placeholder.Domain.Entities;
+
+namespace Placeholder.Domain.Repositories
+{
+    public interface ITodoRepository
+    {
+        Task<IEnumerable<Todo>> GetAll(CancellationToken ct = default);
+
+        Task<Todo> Get(int userId, int todoId, CancellationToken ct = default);
+
+        Task<Todo> Save(Todo todo, CancellationToken ct = default);
+
+        Task Delete(int userId, int todoId, CancellationToken = default);
+    }
+}
