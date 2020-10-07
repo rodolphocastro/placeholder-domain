a9a3a40f13c1631d44565d68e6f79847ce89b9e7
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-10-06 22:35:46 -0300

    test: Test domain logic

--- tests/Placeholder.UnitTests/Domain/Aggregates/TodoAggregateTests.cs
+++ tests/Placeholder.UnitTests/Domain/Aggregates/TodoAggregateTests.cs
@@ -11,17 +11,43 @@ namespace Placeholder.UnitTests.Domain.Aggregates
 {
     public class TodoAggregateTests
     {
+        private static TodoAggregate CreateAggregate() => new TodoAggregate(new InMemoryTodoRepository());
+
         [Fact]
         public async Task Annotate_EmptyTodo_ThrowsArgumentNulLExcetpion()
         {
             // Arrange
-            var subject = new TodoAggregate(new InMemoryTodoRepository());
+            var subject = CreateAggregate();
 
             // Act
             Func<Task> act = () => subject.CreateAnotated(Todo.Empty, "A crucial observation");
 
             // Assert
             await Assert.ThrowsAsync<ArgumentNullException>(act);
+        }
+
+
+        [Fact]
+        public async Task Annotate_ValidTodo_ReturnsAnotatedTodo()
+        {
+            // Arrange
+            const string myObservation = "Witsy observation";
+
+            var subject = CreateAggregate();
+            var todo = new Todo
+            {
+                Id = 1,
+                UserId = 1,
+                Completed = true,
+                Title = "My Awesome Todo"
+            };
+
+            // Act
+            var result = await subject.CreateAnotated(todo, myObservation);
+
+            // Assert
+            Assert.NotEqual(Todo.Empty, result);
+            Assert.Equal(myObservation, result.Observation);
         }
     }
 }
